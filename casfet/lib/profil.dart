0cbce007de16e7fbdd8310e8376ec85c379cdbcd
Damar A <damaradjisodikin@gmail.com>
2022-12-23 17:01:37 +0700

    update profil
    nambahin text

--- casfet/lib/profil.dart
+++ casfet/lib/profil.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
+class Profile {
+  var nama;
+  var email;
+  var image;
+
+  Profile({this.nama, this.email, this.image});
+}
+
 class Profil extends StatelessWidget {
-  /*Profil({this.image, this.nama, this.email});
-  final String image;
+  Profile prof = Profile(
+    nama: 'Damar',
+    email: 'damar@gmail.com',
+  );
+  //Profil({this.image, this.nama, this.email});
+  /* final String image;
   final String nama;
   final String email;*/
   @override
@@ -21,6 +33,14 @@ class Profil extends StatelessWidget {
                 backgroundColor: Colors.grey,
                 //backgroundImage: AssetImage(image),
                 radius: 70.0,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: prof.nama,
+                    hintStyle: TextStyle(color: Colors.black)),
               )
             ],
           ),
