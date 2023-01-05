21af62ffd65f44d1c212efc001aab985848fc05e
ihsan shiddiq <71961467+ihsanshiddiq@users.noreply.github.com>
2023-01-06 02:28:31 +0700

    Login logic
    logika login, pakai firebase Auth

--- casfet/lib/main.dart
+++ casfet/lib/main.dart
@@ -1,19 +1,58 @@
 // import 'dart:html';
 import 'splashscreen.dart';
 import 'package:flutter/material.dart';
+// firebase auth
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:casfet/widget/login_widget.dart';
+import './admin/beta-homepage.dart';
+import 'auth_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-      color: Colors.blue,
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        navigatorKey: navigatorKey,
+        debugShowCheckedModeBanner: false,
+        // title: title,
+        // theme: ThemeData.dark().copyWith(),
+        home: MyHomePage(),
+      );
+
+  // {
+  //   return MaterialApp(
+  //     home: MyHomePage(),
+  //   );
+  // }
+}
+
+class MyHomePage extends StatelessWidget {
+  // const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) => Scaffold(
+          body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePageAdmin();
+          } else {
+            return AuthPage();
+          }
+        },
+      ));
+
+  // {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     home: SplashScreen(),
+  //     color: Colors.blue,
+  //   );
+  // }
 }
---
+++ casfet/lib/widget/login_widget.dart
@@ -0,0 +1,104 @@
+import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth_email/main.dart';
+// import 'package:firebase_auth_email/utils/utils.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:casfet/main.dart';
+
+class LoginWidget extends StatefulWidget {
+  final VoidCallback onClickedRegister;
+
+  const LoginWidget({
+    Key? key,
+    required this.onClickedRegister,
+  }) : super(key: key);
+
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 40),
+          TextField(
+            controller: _emailController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          SizedBox(height: 4),
+          TextField(
+            controller: _passwordController,
+            cursorColor: Colors.white,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.fromHeight(50),
+            ),
+            icon: Icon(Icons.lock_open, size: 32),
+            label: Text(
+              'Login',
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: Login,
+          ),
+          SizedBox(height: 24),
+          RichText(
+            text: TextSpan(
+              style: TextStyle(color: Colors.black, fontSize: 14),
+              text: 'Don’t have an account?  ',
+              children: [
+                TextSpan(
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = widget.onClickedRegister,
+                  text: 'Register',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ));
+
+  Future Login() async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Center(child: CircularProgressIndicator()),
+    );
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  }
+}
