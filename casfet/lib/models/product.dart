9dbea734f9df12469d000a322e9c633dff9c0ee6
ihsan shiddiq <71961467+ihsanshiddiq@users.noreply.github.com>
2023-01-13 20:42:42 +0700

    final

---
+++ casfet/lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  String? pid;
+  String? product_name;
+  String? product_quantity;
+  String? product_price;
+  String? product_img;
+
+  Product({
+    this.pid,
+    this.product_name,
+    this.product_quantity,
+    this.product_price,
+    this.product_img,
+  });
+}
---
+++ casfet/lib/user/editproduk.dart
@@ -0,0 +1,180 @@
+import 'list-produk.dart';
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+import '../services/firebase_crud.dart';
+
+class EditPage extends StatefulWidget {
+  final Product? product;
+  EditPage({this.product});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _EditPage();
+  }
+}
+
+class _EditPage extends State<EditPage> {
+  final nama_produk = TextEditingController();
+  final harga_produk = TextEditingController();
+  final stok_produk = TextEditingController();
+  final produk_terjual = TextEditingController();
+  final img_produk = TextEditingController();
+  final _docid = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _docid.value = TextEditingValue(text: widget.product!.pid.toString());
+    nama_produk.value =
+        TextEditingValue(text: widget.product!.product_name.toString());
+    harga_produk.value =
+        TextEditingValue(text: widget.product!.product_price.toString());
+    stok_produk.value =
+        TextEditingValue(text: widget.product!.product_quantity.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final DocIDField = TextField(
+        controller: _docid,
+        readOnly: true,
+        autofocus: false,
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "id",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final nameField = TextFormField(
+        controller: nama_produk,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Name",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+    final positionField = TextFormField(
+        controller: harga_produk,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Harga Produk",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+    final contactField = TextFormField(
+        controller: stok_produk,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Stok Produk",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final viewListbutton = TextButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil<dynamic>(
+            context,
+            MaterialPageRoute<dynamic>(
+              builder: (BuildContext context) => ProductManager(),
+            ),
+            (route) => false, //if you want to disable back feature set to false
+          );
+        },
+        child: const Text('View List of Employee'));
+
+    final SaveButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Theme.of(context).primaryColor,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            var response = await FirebaseCrud.updateProduct(
+                name: nama_produk.text,
+                price: harga_produk.text,
+                stock: stok_produk.text,
+                docId: _docid.text);
+            if (response.code != 200) {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(response.message.toString()),
+                    );
+                  });
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(response.message.toString()),
+                    );
+                  });
+            }
+          }
+        },
+        child: Text(
+          "Update",
+          style: TextStyle(color: Theme.of(context).primaryColorLight),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('FreeCode Spot'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DocIDField,
+                  const SizedBox(height: 25.0),
+                  nameField,
+                  const SizedBox(height: 25.0),
+                  positionField,
+                  const SizedBox(height: 35.0),
+                  contactField,
+                  viewListbutton,
+                  const SizedBox(height: 45.0),
+                  SaveButon,
+                  const SizedBox(height: 15.0),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
